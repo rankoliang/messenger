f8805c6b27ae3aee6b384b951e2de575083f0232
Ranko Liang <rankoliang@gmail.com>
2021-06-30 20:07:01 -0400

    Add styling to unread messages badge

--- client/src/components/Sidebar/Chat.js
+++ client/src/components/Sidebar/Chat.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import { Box, Typography } from '@material-ui/core';
+import { Box, Badge } from '@material-ui/core';
 import { BadgeAvatar, ChatContent } from '../Sidebar';
 import { withStyles } from '@material-ui/core/styles';
-import { setActiveChat } from '../../store/activeConversation';
 import { setActiveConversation } from '../../store/utils/thunkCreators';
 import { connect } from 'react-redux';
 
 const styles = {
   root: {
+    display: 'flex',
     borderRadius: 8,
     height: 80,
     boxShadow: '0 2px 10px 0 rgba(88,133,196,0.05)',
@@ -18,7 +18,24 @@ const styles = {
       cursor: 'grab',
     },
   },
+  badge: {
+    width: '100%',
+  },
 };
+
+const StyledBadge = withStyles(() => ({
+  badge: {
+    fontSize: 16,
+    height: 26,
+    minWidth: 26,
+    borderRadius: 13,
+    top: '50%',
+    fontWeight: 'bold',
+    paddingLeft: 8,
+    paddingRight: 8,
+    right: 26,
+  },
+}))(Badge);
 
 class Chat extends Component {
   handleClick = async (conversation) => {
@@ -27,20 +44,28 @@ class Chat extends Component {
 
   render() {
     const { classes } = this.props;
-    const otherUser = this.props.conversation.otherUser;
+    const { unreadCount, otherUser } = this.props.conversation;
+
     return (
       <Box
         onClick={() => this.handleClick(this.props.conversation)}
         className={classes.root}
       >
-        <BadgeAvatar
-          photoUrl={otherUser.photoUrl}
-          username={otherUser.username}
-          online={otherUser.online}
-          sidebar={true}
-        />
-        <ChatContent conversation={this.props.conversation} />
-        <Typography>{this.props.conversation.unreadCount}</Typography>
+        <StyledBadge
+          color="primary"
+          badgeContent={unreadCount}
+          variant="standard"
+          anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+          className={classes.badge}
+        >
+          <BadgeAvatar
+            photoUrl={otherUser.photoUrl}
+            username={otherUser.username}
+            online={otherUser.online}
+            sidebar={true}
+          />
+          <ChatContent conversation={this.props.conversation} />
+        </StyledBadge>
       </Box>
     );
   }
