d28b1be41d45863af7a02228fded03d6e8e4ccca
Ranko Liang <rankoliang@gmail.com>
2021-07-07 11:58:39 -0400

    Fix incorrect latest message info
    The message set the first message to be the latest message because of
    message order changes.

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -78,7 +78,8 @@ router.get('/', async (req, res, next) => {
       }
 
       // set properties for notification count and latest message preview
-      convoJSON.latestMessageText = convoJSON.messages[0].text;
+      convoJSON.latestMessageText =
+        convoJSON.messages[convoJSON.messages.length - 1].text;
       convoJSON.unreadCount = unreadCounts.get(convoJSON.id) || 0;
       conversations[i] = convoJSON;
     }
